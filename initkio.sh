730780bd3916bd58acb87ccb03fbb29548849304
Gunars Frickauss <gunarsf@gmail.com>
2023-02-02 10:40:26 +0200

    Update initkio.sh

--- initkio.sh
+++ initkio.sh
@@ -12,11 +12,5 @@ IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
 NEW_HOSTNAME="rpi-kio-"$IPO
 echo $CURRENT_HOSTNAME
 echo $NEW_HOSTNAME
-chattr -i /etc/hostname
-echo "$NEW_HOSTNAME">"/etc/hostname"
-chattr -i /etc/hosts
-sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME\$/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
-hostname $NEW_HOSTNAME
-chattr +i /etc/hostname
-#chattr +i /etc/hosts
+hostnamectl set-hostname ${NEW_HOSTNAME} --static
 /sbin/shutdown -r now
