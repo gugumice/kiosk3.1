b4e593936285d916bd405f8596fe3bc545e25d05
Gunars Frickauss <gunarsf@gmail.com>
2023-02-22 14:03:33 +0200

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -21,8 +21,6 @@ usermod -a -G watchdog pi
 service cups restart
 addgroup kiosk
 usermod -a -G kiosk pi
-mkdir /opt/kiosk
-chown pi:kiosk /opt/kiosk
 apt-get --yes install python3-pip
 #apt-get --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install python3-pip
 echo 'KERNEL=="watchdog", MODE="0660", GROUP="watchdog"' > /etc/udev/rules.d/60-watchdog.rules
